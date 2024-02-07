3415e237419155b9c18f5c22e2b80269b9414747
Kevin Kinsey <kevinkinsey2005@gmail.com>
2024-02-07 17:23:15 +0530

    BlumBlumShub: refactoring namespaces and adding templates for dynamic typing

--- Contributors_Only/Source/Generators/blumblumshub.h
+++ Contributors_Only/Source/Generators/blumblumshub.h
@@ -1,9 +1,8 @@
+#pragma once
 #include <iostream>
 #include <numeric>
 #include <cmath>
-
-// For ease of use, defining `data` to represent unsigned long long
-#define data unsigned long long
+#include <type_traits>
 
 namespace BitGen{
     enum BitGenType{
@@ -12,17 +11,25 @@ namespace BitGen{
         PARITY
     };
 
-    int get_lsb(data x){
+    template <typename T>
+    int find_bitlen(T i){
+        // Utility function to find no of bits required to represent `i`
+        return std::ceil(std::log2(i));
+    }
+
+    template <typename T>
+    int get_lsb(T x){
         // Gets least significant bit of `x`
         return x % 2;
     }
 
-    int get_parity_bit(data num) {
+    template <typename T>
+    int get_parity_bit(T num) {
         // Gets the parity bit for `x`
         int count = 0;
 
         // Counting set bits
-        while (num) {
+        while (num > 0) {
             count += num & 1;  // Add 1 if the least significant bit is set
             num >>= 1;        // Right shift to check the next bit
         }
@@ -32,92 +39,96 @@ namespace BitGen{
     }
 }
 
+namespace DiceForge
+{   
+    template <typename T>
+    class BlumBlumShub{
+        // A Pseudo-Random Number Generator using the BlumBlumShub algorithm
+        private:
+            T x, p, q, n;
+            int (*get_bit)(T);
 
-class BlumBlumShub{
-    // A Pseudo-Random Number Generator using the BlumBlumShub algorithm
-    private:
-        data x, p, q, n;
-        int (*get_bit)(data);
-
-        int find_bitlen(data i){
-            // Utility function to find no of bits required to represent `i`
-            return std::ceil(std::log2(i));
-        }
-
-        int generate_bit(){
-            // Propagates the seed and returns bit extracted from current seed
-            x = (x*x) % n;
-            return get_bit(x);
-        }
-
-
-    public:
-        BlumBlumShub(
-                data p, data q, data seed,
-                BitGen::BitGenType type = BitGen::LSB): p(p), q(q), n(p*q), x(seed){
-            // Validating that p and q are both congruent to 3 (mod 4)
-            if ((p % 4 != 3) || (q % 4 != 3)) {
-                std::cerr << "Error: p and q must be congruent to 3 (mod 4)" << std::endl;
-                exit(1);
+            inline void next(){
+                // Propagates the seed
+                x = (x*x) % n;
             }
 
-            // Validating that p and q are co-prime
-            if (std::gcd(p, q) != 1) {
-                std::cerr << "Error: p and q must be co-prime" << std::endl;
-                exit(1);
+            int generate_bit(){
+                next();
+                // returns bit extracted from current seed
+                return get_bit(x);
             }
 
-            // Performing check on seed
-            // Co-prime check
-            if(std::gcd(x, n) != 1) {
-                std::cerr << "Error: Seed is not coprime with `n = p * q`" << std::endl;
-                exit(1);
-            }
-            // Check for specific unviable seeds
-            if(x == 1) {
-                std::cerr << "Error: Seed cannot be 1" << std::endl;
-                exit(1);
-            }
-            if(x == 0) {
-                std::cerr << "Error: Seed cannot be 0" << std::endl;
-                exit(1);
-            }
 
-            // Validating and assigning the bit generation method according to choice
-            switch (type) {
-                case BitGen::LSB:
-                    get_bit = *BitGen::get_lsb;
-                    break;
-
-                case BitGen::PARITY:
-                    get_bit = *BitGen::get_parity_bit;
-                    break;
-                
-                default:
-                    std::cerr << "Error: Invalid bit generator type given" << std::endl;
+        public:
+            BlumBlumShub(
+                    T p, T q, T seed,
+                    BitGen::BitGenType type = BitGen::LSB): p(p), q(q), n(p*q), x(seed){
+                // Validating that p and q are both congruent to 3 (mod 4)
+                if ((p % 4 != 3) || (q % 4 != 3)) {
+                    std::cerr << "Error: p and q must be congruent to 3 (mod 4)" << std::endl;
                     exit(1);
+                }
+
+                // Validating that p and q are co-prime
+                if (std::gcd(p, q) != 1) {
+                    std::cerr << "Error: p and q must be co-prime" << std::endl;
+                    exit(1);
+                }
+
+                // Performing check on seed
+                // Co-prime check
+                if(std::gcd(x, n) != 1) {
+                    std::cerr << "Error: Seed is not coprime with `n = p * q`" << std::endl;
+                    exit(1);
+                }
+                // Check for specific unviable seeds
+                if(x == 1) {
+                    std::cerr << "Error: Seed cannot be 1" << std::endl;
+                    exit(1);
+                }
+                if(x == 0) {
+                    std::cerr << "Error: Seed cannot be 0" << std::endl;
+                    exit(1);
+                }
+
+                // Validating and assigning the bit generation method according to choice
+                switch (type) {
+                    case BitGen::LSB:
+                        get_bit = *BitGen::get_lsb<T>;
+                        break;
+
+                    case BitGen::PARITY:
+                        get_bit = *BitGen::get_parity_bit<T>;
+                        break;
+                    
+                    default:
+                        std::cerr << "Error: Invalid bit generator type given" << std::endl;
+                        exit(1);
+                }
+            };
+
+            template <typename I>
+            I generate_random_number(I min, I max){
+                // Validating range
+                if (min > max) {
+                    std::cerr << "Error: Invalid range" << std::endl;
+                    exit(1);
+                }
+
+                // finding range, [max, min] both inclusive
+                I range = max - min + 1;
+
+                // generating number bit by bit
+                // iteration done for no of bits required for given range
+                I number = 0;
+                for(int i = 0; i < BitGen::find_bitlen(range); i++){
+                    number = (number << 1) | generate_bit();
+                }
+
+                // Tailoring the number to fit range
+                number = number % range + min;
+                return number;
             }
-        };
-
-        data generate_random_number(data min, data max){
-            // Validating range
-            if (min > max) {
-                std::cerr << "Error: Invalid range" << std::endl;
-                exit(1);
-            }
-
-            // finding range, [max, min] both inclusive
-            data range = max - min + 1;
-
-            // generating number bit by bit
-            // iteration done for no of bits required for given range
-            data number = 0;
-            for(int i = 0; i < find_bitlen(range); i++){
-                number = (number << 1) | generate_bit();
-            }
-
-            // Tailoring the number to fit range
-            number = number % range + min;
-            return number;
-        }
-};
+    };
+}
