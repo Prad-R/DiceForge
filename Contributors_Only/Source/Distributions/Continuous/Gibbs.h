b63add67eeee7508e08817ba0c0f03e03f9e28bd
Parijat Mukherjee <parijatmukherjee11@gmail.com>
2024-02-24 17:58:59 +0530

    Change type of Gibbs from double to int

--- Contributors_Only/Source/Distributions/Continuous/Gibbs.h
+++ Contributors_Only/Source/Distributions/Continuous/Gibbs.h
@@ -8,17 +8,17 @@ namespace DiceForge {
         private:
             real_t *cumulative = nullptr;
             real_t *prob = nullptr;
-            real_t *x_array = nullptr;
+            int_t *x_array = nullptr;
             int_t n = 0;
         public:
-            void specify(real_t *x_arr, real_t *func_arr, int len, real_t beta);
-            real_t next(real_t r) override;
-            real_t variance() override;
-            real_t expectation() override;
-            real_t minValue() override;
-            real_t maxValue() override;
-            real_t pmf(real_t x) override;
-            real_t cdf(real_t x) override;
+            void specify(int_t *x_arr, real_t *func_arr, int len, real_t beta);
+            int_t next(real_t r) override;
+            real_t variance() const override;
+            real_t expectation() const override;
+            int_t minValue() const override;
+            int_t maxValue() const override;
+            real_t pmf(int_t x) const override;
+            real_t cdf(int_t x) const override;
     };
 }
 
