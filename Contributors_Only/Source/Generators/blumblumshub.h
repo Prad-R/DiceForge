1e432c550785ca30ee2fc1f0e811ff05635b2b06
theonlynavin <navinkumar.lakshmanaraj@gmail.com>
2024-02-24 15:12:32 +0530

    Split BBS to .h, .cpp

--- Contributors_Only/Source/Generators/blumblumshub.h
+++ Contributors_Only/Source/Generators/blumblumshub.h
@@ -24,62 +24,37 @@ namespace DiceForge
             /**
              * @brief propagate - Advances the internal state using the Blum-Blum-Shub algorithm
              */
-            inline void propagate(){
-                state = (state * state) % n;
-
-                // Ensure the state is not 0 or 1
-                if(state == 0 || state == 1){
-                    state += 4294967387ULL; // A random constant to avoid getting stuck at 0 or 1
-                }
-            }
+            inline void propagate();
 
             /**
              * @brief generate - Generates a random number using the Blum-Blum-Shub algorithm
              * @return T - The generated random number
              */
-            T generate() override {
-                T num = 0;
-                for(int i = 0; i < _genlen; i++){
-                    propagate();
-                    num = (num << 1) | (state % 2); // Extracting LSB and appending
-                }
-                return num;
-            }
+            T generate() override;
 
             /**
              * @brief check_seed - Checks if the seed is coprime with n (p*q)
              * @param seed - The seed value
              */
-            void check_seed(T seed){
-                if (std::gcd(seed, n) != 1)
-                {
-                    std::cerr << "Error: Seed is not coprime with `n = p * q`" << std::endl;
-                    exit(1);
-                }
-            }
+            void check_seed(T seed);
 
             /**
              * @brief reseed - Reseeds the generator with a new seed
              * @param seed - The new seed value
              */
-            void reseed(T seed) override{
-                state = seed;
-            }
+            void reseed(T seed) override;
 
         public:
             /**
              * @brief Constructor for BlumBlumShub
              * @param seed - The initial seed value
              */
-            BlumBlumShub(T seed) : state(seed) {
-                _genlen = sizeof(T) * 8; // Bit length for generation
-                check_seed(seed);
-            }
+            BlumBlumShub(T seed);
 
             /**
              * @brief Destructor for BlumBlumShub
              */
-            ~BlumBlumShub() {}
+            ~BlumBlumShub();
     };
 
     // Typedefs for convenience
