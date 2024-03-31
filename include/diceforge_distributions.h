13010866c43e40c3388306da9d58e656ad91b4be
Aditya <adsaw005@gmail.com>
2024-03-31 11:55:02 +0530

    Modifying CMakeLists.txt and include/diceforge_distributions.h

--- include/diceforge_distributions.h
+++ include/diceforge_distributions.h
@@ -390,6 +390,23 @@ namespace DiceForge {
             /* cdf(x) [Real] - Cumulative distribution function */
             real_t cdf(int_t x) const override;
     };
+
+    /// @brief DiceForge::Geometric - A Discrete Probability Distribution (Geometric) 
+    class Geometric : public Discrete {
+        private:
+            float p;
+        public:
+            Geometric(real_t p);
+	    
+	    
+            int_t next(real_t r);
+            real_t variance() const override final;
+            real_t expectation() const override final;
+            int_t minValue() const override final;
+            int_t maxValue() const override final;
+            real_t pmf(int_t k) const override final;
+            real_t cdf(int_t k) const override final;
+    };
 }
 
 
